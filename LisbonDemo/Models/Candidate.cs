838bbc44170681c9b56f2de4de7ad7eeeaade2dd
cfsimoes <cesar.simoes@hr-manager.net>
2015-04-17 16:33:45 +0100

    ghjkgjk

--- LisbonDemo/Models/Candidate.cs
+++ LisbonDemo/Models/Candidate.cs
@@ -25,7 +25,7 @@ namespace LisbonDemo.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+        public int Id { get; set; } // comment
         public string Name { get; set; }// dummy comment
     }
 }
