a1f3b95d1dc22a332013dd02ea5281788c386ec2
cfsimoes <cesar.simoes@hr-manager.net>
2015-04-17 16:06:25 +0100

    1st checkin to publish

---
+++ LisbonDemo/Controllers/CandidateController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LisbonDemo.Models;
+
+namespace LisbonDemo.Controllers
+{
+    public class CandidateController : Controller
+    {
+        private CandidatesContext db = new CandidatesContext();
+
+        //
+        // GET: /Candidate/
+
+        public ActionResult Index()
+        {
+            return View(db.Candidates.ToList());
+        }
+
+        //
+        // GET: /Candidate/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Candidate candidate = db.Candidates.Find(id);
+            if (candidate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(candidate);
+        }
+
+        //
+        // GET: /Candidate/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Candidate/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Candidate candidate)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Candidates.Add(candidate);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(candidate);
+        }
+
+        //
+        // GET: /Candidate/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Candidate candidate = db.Candidates.Find(id);
+            if (candidate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(candidate);
+        }
+
+        //
+        // POST: /Candidate/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Candidate candidate)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(candidate).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(candidate);
+        }
+
+        //
+        // GET: /Candidate/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Candidate candidate = db.Candidates.Find(id);
+            if (candidate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(candidate);
+        }
+
+        //
+        // POST: /Candidate/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Candidate candidate = db.Candidates.Find(id);
+            db.Candidates.Remove(candidate);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LisbonDemo/Models/Candidate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LisbonDemo.Models
+{
+    public class CandidatesContext : DbContext
+    {
+        public CandidatesContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+
+        public DbSet<Candidate> Candidates { get; set; }
+    }
+
+    [Table("Candidate")]
+    public class Candidate
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
